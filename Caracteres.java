a6c452cb72aa164ae3bd99ebb0bc47f36c33c923
perezlemadavid <perezlemadavid@gmail.com>
2025-10-02 00:20:07 +0200

    Create Caracteres.java

---
+++ Caracteres.java
@@ -0,0 +1,15 @@
+package Practicas;
+
+public class Caracteres {
+
+	public static void main(String[] args) {
+		int mayuscula = 65 + (int)(Math.random()*25);
+		System.out.println((char)mayuscula);
+		int minuscula = 97 + (int)(Math.random()*25);
+		System.out.println((char)minuscula);
+		int numero = 48 + (int)(Math.random()*9);
+		System.out.println((char)numero);
+
+	}
+
+}
